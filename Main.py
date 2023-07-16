7d6929280ee6726dd0bef92990b9fd7548175b5a
ToopKungZa <toopkungza@gmail.com>
2023-07-16 21:54:43 +0700

    UPDATED 660716

--- Main.py
+++ Main.py
@@ -12,16 +12,27 @@ Simple life chaos generator :P
 """
 import random
 
+#777 digits
+
+def get_random_pi_digits(numdigits):    
+    pi_digits = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513200056812714526356082778577134275778960917363717872146844090122495343014654958537105079227968925892354201995611212902196086403441815981362977477130996051870721134999999837297805"
+    startindex = random.randint(0, (len(pi_digits))-(numdigits + 1)) #minus to prevent out of index
+    selecteddigits = pi_digits[startindex:startindex+numdigits]
+    return selecteddigits
+
 def AskType():
     container = 0
     print("What type of loop do you want me to run on? \n")
     print("1. Statisfication Check \n")
     print("2. Times of running \n")
+    print("3. Run based on 5 generated sequences (whole) of 777 decimal points of pi")
     CheckType = int(input("Which one?"))
     if CheckType == 1:
         container = 1
     elif CheckType == 2:
         container = 2
+    elif CheckType == 3:
+        container = 3
     return container
 
 def RandomExecute():
@@ -64,5 +75,20 @@ if ready == "y":
             randomcounter.append(RunTime)
             TimestoRun -= 1
         Summary(randomcounter)
+    elif Checked == 3:
+        randompi = get_random_pi_digits(5)
+        TimestoRun = int(randompi)
+        print("The 5 digits are", randompi)
+        print("Initialzing the chaos generator in the total times of 5 digits. ..")
+        while TimestoRun != 0:
+            print("Generating")
+            RunTime = RandomExecute()
+            randomcounter.append(RunTime)
+            TimestoRun -= 1
+        Summary(randomcounter)
+        
 if ready == "n":
     print("Bye! :)")
+
+
+
