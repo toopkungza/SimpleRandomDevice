3387602c7a7f1220c0e92460eda5b90836571f51
ToopKungZa <91062662+toopkungza@users.noreply.github.com>
2023-07-24 11:36:23 +0700

    Cleanup #1

--- Main.py
+++ Main.py
@@ -15,30 +15,22 @@ import random
 #777 digits
 def GetRandomPiSeq(numdigits):    
     pi_digits = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513200056812714526356082778577134275778960917363717872146844090122495343014654958537105079227968925892354201995611212902196086403441815981362977477130996051870721134999999837297805"
-    startindex = random.randint(0, (len(pi_digits)-(numdigits))) #minus to prevent out of index
-    selecteddigits = pi_digits[startindex:(startindex+numdigits)]
+    selecteddigits = "0"
     while selecteddigits[0] == "0": #to prevent the start of 0 in digits, which results in only 4 sequences 
         startindex = random.randint(0, (len(pi_digits)-(numdigits)))
         selecteddigits = pi_digits[startindex:(startindex+numdigits)]
     return selecteddigits
 
 def AskType():
-    container = 0
     print("What type of loop do you want me to run on? \n")
     print("1. Statisfication Check \n")
     print("2. Times of running \n")
     print("3. Run based on the total times of the 5 sequences randomly selected from any of 777 decimal points of pi (eg. 14159 or 97932 or 97494). \n")
     CheckType = int(input("Which one? \n"))
-    if CheckType == 1:
-        container = 1
-    elif CheckType == 2:
-        container = 2
-    elif CheckType == 3:
-        container = 3
-    return container
+    return CheckType
 
 def RandomExecute():
-    
+    print("Generating...")
     randomx = random.random()
     if randomx > 0.5:
         print("Yes/Left/Start/Go ahead. This equals to 1. \n")
@@ -80,15 +72,11 @@ if ready == "y":
     elif Checked == 3:
         randompi = GetRandomPiSeq(5)
         TimestoRun = int(randompi)
-        print("The 5 digits are", randompi)
-        print("Initialzing the chaos generator in the total times of 5 digits. ..")
         while TimestoRun != 0:
-            print("Generating")
             RunTime = RandomExecute()
             randomcounter.append(RunTime)
             TimestoRun -= 1
         Summary(randomcounter)
-        
 if ready == "n":
     print("Bye! :)")
 
